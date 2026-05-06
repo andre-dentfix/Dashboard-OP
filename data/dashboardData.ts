feeb0be75339e1abb30c705e9039af1d1553ca22
andre-dentfix <andre@dentfix.com.br>
2026-05-06 11:10:43 -0300

    Create dashboardData.ts

---
+++ data/dashboardData.ts
@@ -0,0 +1,15 @@
+export type MonthlyDashboardData = {
+  month: string
+  totalOps: number
+  conformes: number
+  naoConformes: number
+}
+
+export const monthlyDashboardData: MonthlyDashboardData[] = [
+  {
+    month: "Abr/26",
+    totalOps: 244,
+    conformes: 192,
+    naoConformes: 49,
+  },
+]
